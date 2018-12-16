3619dba554d96830243d2e7f29e5d4192cad0f1c
bluepostit <32864499+bluepostit@users.noreply.github.com>
2018-12-16 16:46:36 +0200

    model validation

--- hangman/forms.py
+++ hangman/forms.py
@@ -1,7 +1,17 @@
 from django import forms
 
+def validate_guess(data):
+    guess = str(data).strip()
+    if len(guess) < 1:
+        raise forms.ValidationError("Guess cannot be empty!")
+    if not guess.isalpha():
+        raise forms.ValidationError(
+            "Guess must contain letters only!")
+
+
 class GuessForm(forms.Form):
-    guess = forms.CharField(max_length=20)
+    guess = forms.CharField(max_length=20,
+                            validators=[validate_guess])
 
     def __init__(self, *args, **kwargs):
         super(GuessForm, self).__init__(*args, **kwargs)
